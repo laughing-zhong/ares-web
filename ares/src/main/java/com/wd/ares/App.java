2a95c2945afcc52ba41ee366bf472d12d5160441
laughing-zhong <wiqi.zhong@gmail.com>
2016-07-22 18:29:06 +0800

    update

--- ares/src/main/java/com/wd/ares/App.java
+++ ares/src/main/java/com/wd/ares/App.java
@@ -1,5 +1,7 @@
 package com.wd.ares;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        ReentrantLock  lock = new ReentrantLock();
+        lock.lock();
+        lock.unlock();
+
     }
 }
--- ares/src/main/java/com/wd/ares/controller/WdController.java
+++ ares/src/main/java/com/wd/ares/controller/WdController.java
@@ -1,6 +1,13 @@
 package com.wd.ares.controller;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.inject.Inject;
+import javax.sql.DataSource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,17 +24,21 @@ import com.wd.ares.db.service.DbUtilsTemplate;
 @Controller
 public class WdController {
 	
-	
 	@Inject
-	private DbUtilsTemplate  dbUtilsTmpl;
+	 private DataSource dataSource;  
 	
 	@RequestMapping(value = "/wd/user",method = {RequestMethod.POST, RequestMethod.GET})
 	@ResponseBody	
-	public WUser  getUser(){
-		WUser user = new WUser();
-		//dbUtilsTmpl.find(WUser.class, sql, param)
-		return user;
-		
+	public String getUser() throws SQLException {
+		Connection con = dataSource.getConnection();
+		CallableStatement cstmt = con.prepareCall("{call dbo.wdProc_k3wsi_allprocess(?,?,?)}");
+		cstmt.setString(1, "201");
+		cstmt.setString(2, "AA");
+		cstmt.registerOutParameter(3, java.sql.Types.CHAR);
+		cstmt.execute();
+		String ret = cstmt.getNString(3);
+		System.out.println("MANAGER ID: " + ret);
+		return ret;
 	}
 
 }
