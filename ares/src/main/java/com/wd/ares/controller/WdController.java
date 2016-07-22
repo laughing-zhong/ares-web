3629ef822f1ef07a99498ad3c998b9230c3cf9c8
laughing-zhong <wiqi.zhong@gmail.com>
2016-07-22 20:22:10 +0800

    update

--- ares/src/main/java/com/wd/ares/controller/WdController.java
+++ ares/src/main/java/com/wd/ares/controller/WdController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.wd.ares.bean.WdRequestBean;
+import com.wd.ares.bean.WdRequsetBeanList;
+import com.wd.ares.db.utils.BeanUtils;
 
 
 
@@ -24,13 +25,15 @@ public class WdController {
 	@Inject
 	 private DataSource dataSource;  
 	
-	@RequestMapping(value = "/wd/user",method = {RequestMethod.POST, RequestMethod.GET})
+	@RequestMapping(value = "/wd",method = {RequestMethod.POST, RequestMethod.GET})
 	@ResponseBody	
-	public String getUser(WdRequestBean reqiestBean) throws SQLException {
+	public String getUser(WdRequsetBeanList reqiestBean) throws SQLException {
 		Connection con = dataSource.getConnection();
+		
+		String insertValues = BeanUtils.concertBeanToInsertValue(reqiestBean);
 		CallableStatement cstmt = con.prepareCall("{call dbo.wdProc_k3wsi_allprocess(?,?,?)}");
 		cstmt.setString(1, "201");
-		cstmt.setString(2, "AA");
+		cstmt.setString(2, insertValues);
 		cstmt.registerOutParameter(3, java.sql.Types.CHAR);
 		cstmt.execute();
 		String ret = cstmt.getNString(3);
--- ares/src/main/java/com/wd/ares/db/utils/BeanUtils.java
+++ ares/src/main/java/com/wd/ares/db/utils/BeanUtils.java
@@ -1,15 +1,16 @@
 package com.wd.ares.db.utils;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.List;
 
 import com.wd.ares.bean.WdRequestBean;
 
 public class BeanUtils {
-	public static String concerToInsertValue(Object msgBean) {
+	public static String concertBeanToInsertValue(Object msgBean) {
 		Field[] fields = msgBean.getClass().getDeclaredFields();
 		StringBuilder sb = new StringBuilder();
+		sb.append("(");
 		try{
 			for(int i = 0 ; i < fields.length; ++i){
 				Field field = fields[i];
@@ -24,17 +25,24 @@ public class BeanUtils {
 				sb.append(",");
 				
 			}
-			return sb.substring(0, sb.length() - 1);		
+			sb.deleteCharAt(sb.length() - 1);
+			sb.append(")");
+			return sb.toString();		
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		return null;
 	}
 	
+	public static String concertBeanListToInsertValue(List<Object> msgBeans){
+	
+		return null;
+	}
+	
 	public static void main(String[] args) {
 		WdRequestBean  msgBean = new WdRequestBean();
 		msgBean.setFEntryID_SRC("aaa");
-		String values = concerToInsertValue(msgBean);
+		String values = concertBeanToInsertValue(msgBean);
 		System.out.println("valued = " + values);
 	}
 
