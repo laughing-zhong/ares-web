8c77698eebc0655b56ca0ccb93901aadc111bbfd
laughing-zhong <wiqi.zhong@gmail.com>
2016-08-08 11:19:36 +0800

    add read body from file

--- ares/src/main/java/com/wd/ares/httpclient/AresHttpClient.java
+++ ares/src/main/java/com/wd/ares/httpclient/AresHttpClient.java
@@ -13,10 +13,12 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wd.ares.bean.WdBaseRequest;
 import com.wd.ares.bean.WdRequest;
 import com.wd.ares.bean.WdRequsetBeanList;
+import com.wd.ares.utils.AresFileReader;
 
 public class AresHttpClient {
 	
@@ -45,19 +47,52 @@ public class AresHttpClient {
         response.close();
 	}
 	
+	public static void callRpcMethod(String url,byte[] body, String rpcService, String method) throws ClientProtocolException, IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		WdBaseRequest request = new WdBaseRequest();
+		request.setRpcService(rpcService);
+		request.setRpcMethod(method);
+		request.setPayLoad(body);
+		
+		
+		HttpClientBuilder httpClientBuilder = HttpClientBuilder.create(); 
+	    //HttpClient  
+        CloseableHttpClient closeableHttpClient = httpClientBuilder.build(); 
+        HttpPost httpPost = new HttpPost(url); 
+        httpPost.setHeader("Content-Type", "application/json");
+        httpPost.setEntity(new ByteArrayEntity(objectMapper.writeValueAsBytes(request)));
+        CloseableHttpResponse response =  closeableHttpClient.execute(httpPost);
+        HttpEntity entity = response.getEntity();  
+        if (entity != null) {  
+            System.out.println("--------------------------------------");  
+            System.out.println("Response content: " + EntityUtils.toString(entity, "UTF-8"));  
+            System.out.println("--------------------------------------");  
+        }
+        response.close();
+	}
+	
 	public static void main(String[] args) throws ClientProtocolException, IOException{
 		
 		String url = "http://localhost:8080/wd/rpc";
-		List<WdRequest> requestList = new ArrayList<WdRequest>();
-		for(int i = 0 ; i < 3; i ++){
-			WdRequest  request = new WdRequest();
-			request.setFauxPropId("aaaa");
-			requestList.add(request);
-		}
-		WdRequsetBeanList reqestBeanList = new WdRequsetBeanList();
-		reqestBeanList.setWdReqValues(requestList);
+//		List<WdRequest> requestList = new ArrayList<WdRequest>();
+//		for(int i = 0 ; i < 3; i ++){
+//			WdRequest  request = new WdRequest();
+//			request.setFauxPropId("aaaa");
+//			requestList.add(request);
+//		}
+//		WdRequsetBeanList reqestBeanList = new WdRequsetBeanList();
+//		reqestBeanList.setWdReqValues(requestList);
 		
-		callRpcMethod(url, reqestBeanList,"MaterialService", "meterialSave");
+		if(args.length < 3){
+			System.out.println("please input [serviceName] [methodName] [fileName] in the cmd lines");
+			return;
+		}
+		String serviceName = args[0];
+		String methodName  = args[1];
+		String fileName    = args[2];
+		
+		byte[] body = AresFileReader.readFileByBytes(fileName);
+		callRpcMethod(url, body,serviceName, methodName);
 	}
 	   
 
